7c82bc5d5dc193546d183f8dd59733ad7f876ac8
coder-shivam <innocentboy9472@gmail.com>
2025-06-21 17:44:16 +0530

    Update summary_text.py

--- summary_text.py
+++ summary_text.py
@@ -8,15 +8,14 @@ gen=summar()
 st.title("✍ Text Summarization App")
 i=st.chat_input(" Enter Your Sentences 📌")
 l=st.slider("Choose max lenght Word of summary",30,200,40,step=5)
-b=st.button(" View Summary")
-if b:
-  if i:
-    with st.spinner(" Generating Summary ✍ ...."):
-      set_seed(42)
-      result=gen(i,max_length=l,num_sequences=1)
-      st.success("#### Genterated Summary ####")
-      st.write(result[0])
-  else:
-    st.info("Please Enter Sentences")
+
+if i:
+  with st.spinner(" Generating Summary ✍ ...."):
+    set_seed(42)
+    result=gen(i,max_length=l,num_sequences=1)
+    st.success("#### Genterated Summary ####")
+    st.write(result[0])
+else:
+  st.info("Please Enter Sentences")
     
 
