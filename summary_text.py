9115171c0140082b1da57395f306aa3620e03401
coder-shivam <innocentboy9472@gmail.com>
2025-06-21 17:50:45 +0530

    Update summary_text.py

--- summary_text.py
+++ summary_text.py
@@ -10,10 +10,10 @@ i=st.chat_input(" Enter Your Sentences 📌")
 l=st.slider("Choose max lenght Word of summary",30,200,40,step=5)
 
 if i:
-  with st.spinner(" Generating Summary ✍ ...."):
+  with st.spinner(" Generating Summary ✍ ......."):
     set_seed(42)
     result=gen(i,max_length=l,num_return_sequences=1)
-    st.success("#### Genterated Summary ####")
+    st.success("✔ Genterated Summary ")
     st.write(result[0]['summary_text'])
 else:
   st.info("Please Enter Sentences")
