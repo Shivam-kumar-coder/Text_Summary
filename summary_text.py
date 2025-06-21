66b671b2e81e369201a1283d174d2379d94fe7cb
coder-shivam <innocentboy9472@gmail.com>
2025-06-21 17:36:17 +0530

    Create summary_text.py

---
+++ summary_text.py
@@ -0,0 +1,21 @@
+import streamlit as st 
+from transformers import pipeline,set_seed
+
+@st.cache_resource
+def summar():
+  return pipeline('summarization',model='sshleifer/distilbart-cnn-12-6')
+gen=summar()
+st.title("✍ Text Summarization App")
+i=st.chat_input(" Enter Your Sentences 📌")
+l=st.slider("Choose max lenght Word of summary",30,200,40,step=5)
+b=st.button(" View Summary")
+if b:
+  if i:
+    with st.spinner(" Generating Summary ✍ ...."):
+      result=gen(i,max_length=l,num_sequences=1)
+      st.success("#### Genterated Summary ####")
+      st.write(result[0])
+  else:
+    st.info("Please Enter Sentences")
+    
+
